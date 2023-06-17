9bf88fd5caa9fbdbae70b8cf45d91e2b11dddd03
Flame <5017_30@mail.ru>
2023-06-17 17:08:14 +0300

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,18 @@ import React from 'react'
 import './App.scss'
 import 'swiper/swiper.min.css'
 import './assets/boxicons-2.0.7/css/boxicons.min.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MyRoutes from './config/Routes'
-import Catalog from './pages/Catalog'
-import Detail from './pages/Detail'
-import Home from './pages/Home'
+
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path='/:category/search/:keyword' element={<Catalog />} />
-				<Route path='/:category/:id' element={<Detail />} />
-				<Route path='/' exact element={<Home />} />
-				<Route path='/:category' element={<Catalog />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
+			<>
+				<Header />
+				<MyRoutes/>
+				<Footer />
+			</>
 	)
 }
 
