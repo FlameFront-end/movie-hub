effd9b2041e8f501cffb845d2d437cbed0b5dda2
Flame <5017_30@mail.ru>
2023-06-17 17:12:32 +0300

    fix: rename

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -49,7 +49,7 @@ const Header = () => {
 			<div className='header__wrap container'>
 				<div className='logo'>
 					<img src={logo} alt='logo' />
-					<Link to='/'>tMovies</Link>
+					<Link to='/'>MovieHub</Link>
 				</div>
 				<ul className='header__nav'>
 					{headerNav.map((el, index) => (
