426cff2bca7442dbf90a8d3d45bfada3606a4134
Flame <5017_30@mail.ru>
2023-06-18 13:13:21 +0300

    feat: add footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
 		<div ref={headerRef} className='header'>
 			<div className='header__wrap container'>
 				<div className='logo'>
-					<img src={logo} alt='' />
+					<img src={logo} alt='' logo />
 					<Link to='/'>MovieHub</Link>
 				</div>
 				<ul className='header__nav'>
