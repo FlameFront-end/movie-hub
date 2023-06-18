a4d61c8f73a0b5a60724d3d439ec9a9028ae4794
Flame <5017_30@mail.ru>
2023-06-18 13:13:54 +0300

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import './Footer.scss'
+
+import bg from '../../assets/footer-bg.jpg'
+import logo from '../../assets/tmovie.png'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='footer' style={{ backgroundImage: `url(${bg})` }}>
+			<div className='footer__content container'>
+				<div className='footer__content__logo'>
+					<div className='logo'>
+						<img src={logo} alt='logo' />
+						<Link to='/'>MovieHub</Link>
+					</div>
+				</div>
+				<div className='footer__content__menus'>
+					<div className='footer__content__menu'>
+						<Link to='/'>Home</Link>
+						<Link to='/'>Home</Link>
+						<Link to='/'>Contact us</Link>
+						<Link to='/'>Term of services</Link>
+						<Link to='/'>About us</Link>
+					</div>
+					<div className='footer__content__menu'>
+						<Link to='/'>Live</Link>
+						<Link to='/'>FAQ</Link>
+						<Link to='/'>Premium</Link>
+						<Link to='/'>Term of services</Link>
+						<Link to='/'>Privacy policy</Link>
+					</div>
+					<div className='footer__content__menu'>
+						<Link to='/'>You mus watch</Link>
+						<Link to='/'>Recent release</Link>
+						<Link to='/'>Top IMDB</Link>
+						<Link to='/'>FlameFront-end</Link>
+					</div>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
